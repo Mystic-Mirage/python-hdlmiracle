19476a13f57743bdf62db8a9bdefa49097ad19d3
Mystic-Mirage <mm@m10e.net>
2013-10-16 14:53:30 +0300

    * initial commit

---
+++ smartbus/packet.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+from future.builtins import *  # @UnusedWildImport
+
+from ipaddress import IPv4Address
+import struct
+
+
+class Packet(object):
+
+    def __init__(self, src_netid=3, src_devid=254, src_devtype=65534,
+            op_code=0x000e, dst_netid=255, dst_devid=255, data=[],
+            source_ip='127.0.0.1', hdlmiracle=False):
+        self.src_netid = src_netid
+        self.src_devid = src_devid
+        self.src_devtype = src_devtype
+        self.op_code = op_code
+        self.dst_netid = dst_netid
+        self.dst_devid = dst_devid
+        self.data = data
+        self.source_ip = IPv4Address(source_ip)
+        self._big = False
+        self.hdlmiracle = hdlmiracle
+
+    @property
+    def is_big(self):
+        return self._big
+
+    @property
+    def crc(self):
+        src_devtype = bytearray(struct.pack(b'!H', self.src_devtype))
+        op_code = bytearray(struct.pack(b'!H', self.op_code))
+        data = ([self.length, self.src_netid, self.src_devid] +
+            list(src_devtype) + list(op_code) +
+            [self.dst_netid, self.dst_devid] + self.data)
+        checksum = 0
+        for i in data:
+            checksum = checksum ^ (i << 8)
+            for _ in range(8):
+                if (checksum & 0x8000) > 0:
+                    checksum = (checksum << 1) ^ 0x1021
+                else:
+                    checksum = checksum << 1
+            checksum = checksum & 0xffff
+        return checksum
+
+    @property
+    def length(self):
+        return len(self.data) + (10 if self.is_big else 11)
+
+    @property
+    def op_code_hex(self):
+        return format(self.op_code, '#06x')
+
+    @op_code_hex.setter
+    def op_code_hex(self, value):
+        if type(value) is int:
+            self.op_code = value
+        else:
+            self.op_code = int(value, 16)
+
+    @property
+    def packed(self):
+        src_ip = bytearray(self.source_ip.packed)
+        src_id = bytearray([self.src_netid, self.src_devid])
+        src_devtype = bytearray(struct.pack(b'!H', self.src_devtype))
+        op_code = bytearray(struct.pack(b'!H', self.op_code))
+        dst_id = bytearray([self.dst_netid, self.dst_devid])
+        if self.hdlmiracle:
+            head0 = bytearray(b'HDLMIRACLE')
+        else:
+            head0 = bytearray(b'SMARTCLOUD')
+        head = bytearray([0xaa, 0xaa, self.length])
+        data = bytearray(self.data)
+        crc = bytearray(struct.pack(b'!H', self.crc))
+        return (src_ip + head0 + head + src_id + src_devtype + op_code +
+            dst_id + data + crc)
+
+    @packed.setter
+    def packed(self, raw_packet):
+        packet = bytearray(raw_packet)
+        self.source_ip = IPv4Address('.'.join(map(str, list(packet[:4]))))
+        if (not packet[4:].startswith(b'SMARTCLOUD') and
+            not packet[4:].startswith(b'HDLMIRACLE')):
+            raise Exception('Not SmartBus packet')
+        self._big = True if packet[16] == 0xff else False
+        if not self.is_big and len(packet) != packet[16] + 16:
+            raise Exception('Wrong packet length (%s). Expected value is %s'
+                            % (packet[16], len(packet)))
+        else:
+            if self.is_big:
+                packet_body = packet[17:]
+            else:
+                packet_body = packet[17:-2]
+            self.src_netid = packet_body[0]
+            self.src_devid = packet_body[1]
+            self.src_devtype = packet_body[2] << 8 | packet_body[3]
+            self.op_code = packet_body[4] << 8 | packet_body[5]
+            self.dst_netid = packet_body[6]
+            self.dst_devid = packet_body[7]
+            if self.is_big:
+                big_len = packet_body[8] << 8 | packet_body[9]
+                self.data = list(bytearray(packet_body[10:]))
+                if big_len != self.length:
+                    raise Exception('Wrong packet length (%s). Expected %s'
+                                    % (big_len, self.length))
+            else:
+                self.data = list(packet_body[8:])
+                if packet[-2] << 8 | packet[-1] != self.crc:
+                    raise Exception('Wrong checksum')
+
+
+if __name__ == '__main__':
+    p1 = Packet(source_ip='192.168.10.82', src_netid=0, src_devid=0, data=[1])
+    p2 = Packet()
+    p2.packed = p1.packed
+    print(list(p2.packed))
---
+++ smartlistener.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+from future.builtins import *  # @UnusedWildImport
+
+from os import linesep
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+from smartbus.packet import Packet
+
+
+class SmartListener(DatagramProtocol):
+
+    def startProtocol(self):
+        print('net_id  dev_id  dev_type  op_code  dst_net  dst_dev  data')
+        self.transport.joinGroup('224.0.0.1', interface='127.0.0.1')
+
+    def datagramReceived(self, data, host_port):
+        packet = Packet()
+        packet.packed = data
+        datarepr = []
+        for d in range(0, len(packet.data), 8):
+            hexdata = []
+            asciidata = []
+            for i in packet.data[d:d + 8]:
+                hexdata.append(format(i, '02x'))
+                asciidata.append(chr(i) if i >= 0x20 and i < 0x7f else '.')
+            datarepr.append(' '.join(hexdata).ljust(25) + ''.join(asciidata))
+        datarepr_t = (linesep + ' ' * 55).join(datarepr)
+        print('%-3d     %-3d     %-5d     %s   %-3d      %-3d     %s'
+              % (packet.src_netid, packet.src_devid, packet.src_devtype,
+                 packet.op_code_hex, packet.dst_netid, packet.dst_devid,
+                 datarepr_t))
+
+
+if __name__ == '__main__':
+
+    reactor.listenMulticast(6000, SmartListener())  # @UndefinedVariable
+    reactor.run()  # @UndefinedVariable
