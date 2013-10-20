1e3fcd0f8d10649fa2eda8db42baf72d6c2050e3
Mystic-Mirage <mm@m10e.net>
2013-10-21 00:58:27 +0300

    * start work on packater

--- smartbus/worker.py
+++ smartbus/worker.py
@@ -3,55 +3,95 @@ from __future__ import division, print_function, unicode_literals
 from future.builtins import *  # @UnusedWildImport
 from future import standard_library  # @UnusedImport
 
-from queue import Queue
-import socket
+from queue import Empty, Queue
+from socket import socket, timeout
+import sqlite3
 from threading import Event, Thread
 
+from .packet import Packet
 
-class _Listener(Thread):
 
-    def __init__(self, lp_queue, timeout=1):
-        super().__init__()
-        self.lp_queue = lp_queue
-        self.timeout = timeout
-        self.exit = Event()
+class Receiver(Thread):
+
+    def get(self):
+        try:
+            return self.queue.get_nowait()
+        except Empty:
+            return None
 
     def run(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.exit = Event()
+        self.queue = Queue()
+
+        sock = socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.settimeout(self.timeout)
+        sock.settimeout(1)
         sock.bind(('', 6000))
 
         while not self.exit.is_set():
             try:
                 raw_packet = sock.recv(2048)
-            except:
+            except timeout:
                 pass
             else:
-                self.lp_queue.put(raw_packet)
+                self.queue.put(raw_packet)
 
-        self.lp_queue.put(None)
         sock.close()
 
     def stop(self):
         self.exit.set()
 
 
-class _Parser(Thread):
+class Packeter(Thread):
 
-    def __init__(self, lp_queue, pp_queue):
-        super().__init__()
-        self.lp_queue = lp_queue
-        self.pp_queue = pp_queue
-        self.exit = Event()
+    def put(self, packet):
+        self.queue.put('INSERT ')
 
     def run(self):
+        self.exit = Event()
+        self.queue = Queue()
+
+        self.connection = sqlite3.connect(':memory:')
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute(
+            'CREATE TABLE packets ('
+            'id INTEGER PRIMARY KEY, '
+            'src_netid, '
+            'src_devid, '
+            'src_devtype, '
+            'op_code, '
+            'dst_netid, '
+            'dst_devid, '
+            'data, '
+            'source_ip, '
+            'big BOOLEAN, '
+            'hdlmiracle BOOLEAN, '
+            'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, '
+            ')'
+        )
+
+        self.connection.close()
+
+    def stop(self):
+        self.exit.set()
+
+
+class Parser(Thread):
+
+    def __init__(self, packeter, receiver):
+        super().__init__()
+        self.receiver = receiver
+        self.packeter = packeter
+
+    def run(self):
+        self.exit = Event()
+
         while not self.exit.is_set():
-            raw_packet = self.lp_queue.get()
-            if raw_packet is None:
-                break
-            print(raw_packet)
+            raw_packet = self.receiver.get()
+            if raw_packet is not None:
+                self.packeter.put(Packet(raw_packet))
 
     def stop(self):
         self.exit.set()
@@ -60,15 +100,16 @@ class _Parser(Thread):
 class Worker(object):
 
     def __init__(self):
-        self.lp_queue = Queue()
-        self.pp_queue = Queue()
-        self.listener = _Listener(self.lp_queue)
-        self.parser = _Parser(self.lp_queue, self.pp_queue)
+        self.receiver = Receiver()
+        self.packeter = Packeter()
+        self.parser = Parser(self.packeter, self.receiver)
 
     def start(self):
+        self.receiver.start()
+        self.packeter.start()
         self.parser.start()
-        self.listener.start()
 
     def stop(self):
-        self.listener.stop()
+        self.receiver.stop()
+        self.packeter.stop()
         self.parser.stop()
