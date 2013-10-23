33a54c62a8dbea52f24625b1e591b3bb238f644d
Mystic-Mirage <mm@m10e.net>
2013-10-23 22:39:59 +0300

    * remove twisted dependency

--- smartlistener.py
+++ smartlistener.py
@@ -4,31 +4,48 @@ from future.builtins import *  # @UnusedWildImport
 
 from os import linesep
 
-from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor
+import socket
 
 from smartbus.packet import Packet
 
 
-class SmartListener(DatagramProtocol):
+if __name__ == '__main__':
 
-    def startProtocol(self):
-        print(
-            'netid devid devtype opcode dstnet dstdev hex                     '
-            'ascii')
-        self.transport.joinGroup('224.0.0.1')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.settimeout(1)
+    sock.bind(('', 6000))
 
-    def datagramReceived(self, data, host_port):
-        packet = Packet(data)
+    print(
+        'netid devid devtype opcode dstnet dstdev hex                     '
+        'ascii')
+
+    while True:
+
+        try:
+            raw_packet = sock.recv(2048)
+        except socket.timeout:
+            continue
+        except KeyboardInterrupt:
+            break
+
+        packet = Packet(raw_packet)
+
         datarepr = []
+
         for d in range(0, len(packet.data), 8):
             hexdata = []
             asciidata = []
+
             for i in packet.data[d:d + 8]:
                 hexdata.append(format(i, '02x'))
                 asciidata.append(chr(i) if i >= 0x20 and i < 0x7f else '.')
+
             datarepr.append(' '.join(hexdata).ljust(24) + ''.join(asciidata))
+
         datarepr_t = (linesep + ' ' * 41).join(datarepr)
+
         print(
             '{0.src_netid:-3d}   '
             '{0.src_devid:-3d}   '
@@ -39,8 +56,4 @@ class SmartListener(DatagramProtocol):
             '{1}'.format(packet, datarepr_t)
         )
 
-
-if __name__ == '__main__':
-
-    reactor.listenMulticast(6000, SmartListener())
-    reactor.run()
+    sock.close()
