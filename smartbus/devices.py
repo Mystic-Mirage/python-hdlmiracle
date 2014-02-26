82b50523d2f5ce1f45d375fdbddbd0d74f9a9bdd
Mystic-Mirage <mm@m10e.net>
2014-02-26 09:30:42 +0200

    Begin work on devices

---
+++ smartbus/devices.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+from future.builtins import *
+
+from . import Device, OC_CHANNEL_CONTROL, Packet, sendmethod
+
+
+ALL_CHANNELS = 255
+CHANNEL_OFF = 0
+CHANNEL_ON = 100
+
+
+class DimmerRelay(Device):
+
+    def __init__(self, netid, devid, channels=[]):
+        super().__init__(netid=netid, devid=devid)
+        self._channels = {k: v for k, v in enumerate(channels, start=1)}
+
+    def receive_func(self, packet):
+        if packet.opcode == OC_CHANNEL_CONTROL:
+            self._channels[packet.data[0]] = packet.data[1]
+
+    @sendmethod
+    def _set_channel(self, channel, value):
+        self._channels[channel] = value
+        return Packet(bytearray((channel, value, 0, 0)), OC_CHANNEL_CONTROL,
+            self.netid, self.devid)
+
+    def channel(self, channel, value=None):
+        if value is None:
+            return self._channels.get(channel)
+        else:
+            self._set_channel(channel, value)
