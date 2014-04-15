9b07b3c5f2b1eb31c4142a56674abf1d8ec0180e
Mystic-Mirage <mm@m10e.net>
2014-04-15 14:55:05 +0300

    Use source port

--- smartbus/_handle.py
+++ smartbus/_handle.py
@@ -78,7 +78,9 @@ class Sender(Thread):
         sock = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
         )
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind(('', 6000))
 
         self.running = True
 
